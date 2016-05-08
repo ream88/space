37aeec80fa1885547db9b52e48ac61144e70209d
Mario Uher <uher.mario@gmail.com>
2016-05-08 17:19:34 +0200

    Use Infinity instead of zeros

---
+++ calculateWidths.js
@@ -0,0 +1,18 @@
+export default (widths, totalWidth) => {
+  const fixed = widths.filter(width => isFinite(width));
+  const flexible = widths.filter(width => !isFinite(width));
+
+  totalWidth = totalWidth - fixed.reduce((sum, width) => sum + width, 0);
+
+  let factor = parseInt(totalWidth / flexible.length);
+  let rest = totalWidth - factor * flexible.length;
+
+  return widths
+    .map(width => {
+      if (!isFinite(width)) {
+        width = factor + rest;
+        rest = 0;
+      }
+      return width;
+    });
+};
--- test.js
+++ test.js
@@ -1,35 +1,17 @@
 import { assert } from "chai";
-
-const calculateWidths = (widths, totalWidth) => {
-  const fixed = widths.filter(width => width > 0);
-  const flexible = widths.filter(width => width === 0);
-
-  totalWidth = totalWidth - fixed.reduce((sum, width) => sum + width, 0);
-
-  let factor = parseInt(totalWidth / flexible.length);
-  let rest = totalWidth - factor * flexible.length;
-
-  return widths
-    .map(width => {
-      if (width === 0) {
-        width = factor + rest;
-        rest = 0;
-      }
-      return width;
-    });
-};
+import calculateWidths from "./calculateWidths";
 
 describe("calculateWidths", () => {
   it("1 flexible gets the whole", () => {
-    assert.deepEqual([100], calculateWidths([0], 100));
+    assert.deepEqual([100], calculateWidths([Infinity], 100));
   });
 
   it("2 flexibles get 50% each", () => {
-    assert.deepEqual([50, 50], calculateWidths([0, 0], 100));
+    assert.deepEqual([50, 50], calculateWidths([Infinity, Infinity], 100));
   });
 
   it("2 flexibles get nearly 50% each", () => {
-    assert.deepEqual([50, 49], calculateWidths([0, 0], 99));
+    assert.deepEqual([50, 49], calculateWidths([Infinity, Infinity], 99));
   });
 
   it("1 fixed gets its desired width", () => {
@@ -41,10 +23,10 @@ describe("calculateWidths", () => {
   });
 
   it("1 fixed and 1 flexible sum up to 100", () => {
-    assert.deepEqual([1, 99], calculateWidths([1, 0], 100));
+    assert.deepEqual([1, 99], calculateWidths([1, Infinity], 100));
   });
 
   it("2 fixed and 1 flexible sum up to 100", () => {
-    assert.deepEqual([1, 98, 1], calculateWidths([1, 0, 1], 100));
+    assert.deepEqual([1, 98, 1], calculateWidths([1, Infinity, 1], 100));
   });
 });
